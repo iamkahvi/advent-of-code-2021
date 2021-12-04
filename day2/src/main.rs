3fb5aee185f3f00b93ed96d7408a6ea977a0e965
Kahvi Patel <Kahvi@MacBook-Pro-3.local>
2021-12-04 00:13:38 -0800

    day 2 done

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,29 +2,52 @@ use std::fs;
 
 fn main() {
     let temp = fs::read_to_string("input.txt").unwrap();
-    
     let input = temp
         .lines()
-        .map(|x| x.split(" ").collect())
-        .collect::<Vec<Vec<&str>>>();
+        .map(|x| {
+            let pair = x.split(" ").collect::<Vec<&str>>();
+            return (pair[0], pair[1].parse().unwrap());
+        })
+        .collect::<Vec<(&str, i32)>>();
 
-    println!("{:?}", part_1(&input));
+    println!("part 1: {:?}", part_1(&input));
+    println!("part 2: {:?}", part_2(&input));
 }
 
-fn part_1(input: &Vec<Vec<&str>>) -> i32 {
+fn part_1(input: &Vec<(&str, i32)>) -> i32 {
     let mut depth = 0;
     let mut pos = 0;
-    
     for el in input {
-        let num: i32 = el[1].parse().unwrap();
-        
-        match el[0] {
-            "up" => depth -= num,
-            "down" => depth += num,
-            "forward" => pos += num,
-            _ => println!("Invalid entry")
+        let (cmd, amount) = el;
+        match cmd.as_ref() {
+            "up" => depth -= amount,
+            "down" => depth += amount,
+            "forward" => pos += amount,
+            _ => println!("Invalid entry"),
         }
     }
-    
+    return depth * pos;
+}
+
+fn part_2(input: &Vec<(&str, i32)>) -> i32 {
+    let mut depth = 0;
+    let mut pos = 0;
+    let mut aim = 0;
+    for el in input {
+        let (cmd, amount) = el;
+        match cmd.as_ref() {
+            "up" => {
+                aim -= amount;
+            }
+            "down" => {
+                aim += amount;
+            }
+            "forward" => {
+                pos += amount;
+                depth += aim * amount;
+            }
+            _ => println!("Invalid entry"),
+        }
+    }
     return depth * pos;
 }
