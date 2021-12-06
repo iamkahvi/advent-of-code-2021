e79ffc539c05f6f69dc9e7bfdfda7f34b72f382f
Kahvi Patel <Kahvi@MacBook-Pro-3.local>
2021-12-05 23:49:00 -0800

    not pretty, but it works

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -23,6 +23,62 @@ fn main() {
     println!("part 2: {}", part_2(&input));
 }
 
+fn part_1(input: &Vec<Vec<i32>>) -> i32 {
+    let count = generate_count(input, &(0..input.len()).collect());
+    let gamma = bits_to_number(&count);
+    let epsilon = bits_to_number(&flip_bits(&count));
+    return epsilon * gamma;
+}
+
+fn part_2(input: &Vec<Vec<i32>>) -> i32 {
+    let mut ox_index: Vec<usize> = (0..input.len()).collect();
+    let mut co2_index: Vec<usize> = (0..input.len()).collect();
+    let mut i = 0;
+    let mut count;
+    while ox_index.len() > 1 {
+        count = generate_count(input, &ox_index)[i];
+        ox_index = filter_by_bit_pos(input, ox_index, i, count);
+        i += 1;
+    }
+    i = 0;
+    while co2_index.len() > 1 {
+        count = flip_bits(&generate_count(input, &co2_index))[i];
+        co2_index = filter_by_bit_pos(input, co2_index, i, count);
+        i += 1;
+    }
+    let ox_rating = bits_to_number(&input[ox_index[0]]);
+    let co2_rating = bits_to_number(&input[co2_index[0]]);
+
+    return ox_rating * co2_rating;
+}
+
+// Filters vector by a bit: value at a certain position: pos
+fn filter_by_bit_pos(
+    input: &Vec<Vec<i32>>,
+    index: Vec<usize>,
+    pos: usize,
+    value: i32,
+) -> Vec<usize> {
+    return index
+        .into_iter()
+        .filter(|i| input[*i][pos] == value)
+        .collect();
+}
+
+// Returns an array repesenting the most frequent bit by position
+fn generate_count(input: &Vec<Vec<i32>>, index: &Vec<usize>) -> Vec<i32> {
+    let mut count: Vec<i32> = vec![0; input[0].len()];
+    for (i, num) in input.iter().enumerate() {
+        if index.contains(&i) {
+            for (i, d) in num.iter().enumerate() {
+                count[i] += if d == &1 { 1 } else { -1 };
+            }
+        }
+    }
+    return count.iter().map(|x| if x >= &0 { 1 } else { 0 }).collect();
+}
+
+// Flips the bits of a vector
 fn flip_bits(num: &Vec<i32>) -> Vec<i32> {
     return num
         .iter()
@@ -32,52 +88,11 @@ fn flip_bits(num: &Vec<i32>) -> Vec<i32> {
         .collect();
 }
 
+// Returns the decimal number from a vector
 fn bits_to_number(num: &Vec<i32>) -> i32 {
     return num
         .iter()
         .enumerate()
         .map(|(i, x)| (x * BASE.pow((num.len() - 1 - i) as u32)))
         .sum();
-}
-fn part_1(input: &Vec<Vec<i32>>) -> i32 {
-    println!("{:?}", input);
-
-    let count = generate_count(input);
-    println!("count: {:?}", count);
-    let gamma = bits_to_number(&count);
-    let epsilon = bits_to_number(&flip_bits(&count));
-    println!("gamma: {}", gamma);
-    return epsilon * gamma;
-}
-
-fn part_2(input: &Vec<Vec<i32>>) -> i32 {
-    let mut count;
-    let mut i = 0;
-    let mut filtered = input.clone();
-    while filtered.len() > 1 {
-        count = generate_count(&filtered);
-        filtered = filter_by_bit_pos(filtered, i, count[i]);
-        i += 1;
-    }
-    println!(
-        "oxygen generator rating: {:?}",
-        bits_to_number(&filtered[0])
-    );
-    return 0;
-}
-
-// Filters vector by a bit: value at a certain position: pos
-fn filter_by_bit_pos(input: Vec<Vec<i32>>, pos: usize, value: i32) -> Vec<Vec<i32>> {
-    return input.into_iter().filter(|x| x[pos] == value).collect();
-}
-
-// Returns an array repesenting the most frequent bit by position
-fn generate_count(input: &Vec<Vec<i32>>) -> Vec<i32> {
-    let mut count: Vec<i32> = vec![0; input[0].len()];
-    for num in input {
-        for (i, d) in num.iter().enumerate() {
-            count[i] += if d == &1 { 1 } else { -1 };
-        }
-    }
-    return count.iter().map(|x| if x >= &0 { 1 } else { 0 }).collect();
 }
