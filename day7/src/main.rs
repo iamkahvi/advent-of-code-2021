c6624ea6c2158b6970132e2622670d03c0d053e4
Kahvi Patel <kahvi.patel@shopify.com>
2021-12-23 13:47:09 -0500

    ah its not just the mean instead

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -10,15 +10,28 @@ fn main() {
         .collect();
 
     crabs.sort();
-    println!("part 1: {}", part_1(crabs));
+    println!("part 1: {}", part_1(&crabs));
+    println!("part 2: {}", part_2(&crabs));
 }
 
-fn part_1(crabs: Vec<i32>) -> i32 {
+fn part_1(crabs: &Vec<i32>) -> i32 {
     let med = crabs[crabs.len() / 2];
     println!("{}", med);
     let mut fuel = 0;
     for c in crabs {
         fuel += (c - med).abs();
+    }
+    return fuel;
+}
+
+fn part_2(crabs: &Vec<i32>) -> i32 {
+    let mean = (crabs.iter().sum::<i32>() as f32 / crabs.len() as f32).ceil() as i32;
+    let mut fuel = 0;
+    for c in crabs {
+        let n = (c - mean).abs() as f32;
+        let sum = (n.powf(2.0) + n) / 2.0;
+        fuel += sum as i32;
+        // println!("{} to {} -> {}", c, med, sum as f32);
     }
     return fuel;
 }
